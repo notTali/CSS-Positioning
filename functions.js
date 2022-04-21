d43871d9674de5cebbf8a8165d9b2f94bbb4a682
notTali <mtalifhani54@gmail.com>
2022-04-21 18:49:42 +0200

    function that return something!

--- functions.js
+++ functions.js
@@ -1,12 +1,26 @@
-function greet(){
-    console.log("hello world!");    
-}
+// //function declaration...
+// function greet(name, surname){
+//     console.log(`hello ${name} ${surname}!`);    
+// }
 
-//function expression
-const speak = function(){
-    console.log("function expression");
-};
+// //function expression... Works as a variable!
+// const speak = function(){
+//     console.log("function expression");
+// }; //requires a closing semi-colon
 
-greet();
 
-speak();
+// greet("Talifhani", "Mulaudzi");
+
+// speak();
+
+
+// const claculateArea = function(radius){
+//     return 3.14 * radius**2;
+// };
+
+
+// const area = claculateArea(5);
+// // claculateArea(10);
+// console.log(area);
+
+
