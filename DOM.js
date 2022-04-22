a5a3f85088a7876768129969068c8e0a1487af8b
notTali <mtalifhani54@gmail.com>
2022-04-22 19:36:14 +0200

    Create and append an element

--- DOM.js
+++ DOM.js
@@ -1,10 +1,19 @@
 //Interacting with the web page.
 
-const para = document.querySelector(".test > h1").innerText;
+// const para = document.querySelector(".test > h1").innerText;
 
 // console.log(para);
 
-const body = document.querySelector("body");
-const link = body.querySelector("a");
-console.log(link.getAttribute("href"));
-console.log(body);
+// const body = document.querySelector("body");
+// const link = body.querySelector("a");
+// console.log(link.getAttribute("href"));
+// console.log(body);
+
+const section = document.querySelector(".test");
+// console.log(section);
+
+const h1 = document.createElement("h1");
+h1.innerText = "Inner text updated!";
+h1.style.color = "red";
+h1.style.fontSize = "3rem";
+section.append(h1);
