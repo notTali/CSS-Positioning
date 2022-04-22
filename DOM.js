a2a0db1a3ecf1da41edd5ef9cd23f92acd541437
notTali <mtalifhani54@gmail.com>
2022-04-22 19:27:46 +0200

    JS getAttribute

--- DOM.js
+++ DOM.js
@@ -2,4 +2,9 @@
 
 const para = document.querySelector(".test > h1").innerText;
 
-console.log(para);
+// console.log(para);
+
+const body = document.querySelector("body");
+const link = body.querySelector("a");
+console.log(link.getAttribute("href"));
+console.log(body);
