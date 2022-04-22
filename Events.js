b0235a6c658e6d8feeaeee381b1b0d8f2dd5f2fe
notTali <mtalifhani54@gmail.com>
2022-04-22 20:29:45 +0200

    JavaScript events

---
+++ Events.js
@@ -0,0 +1,6 @@
+const parent = document.querySelector(".parent");
+
+const child1 = document.querySelector(".child-1");
+const child2 = document.querySelector(".child-2");
+const child3 = document.querySelector(".child-3");
+
