6fe7696665feb865af285802c10da734977659d0
notTali <mtalifhani54@gmail.com>
2022-04-22 20:51:20 +0200

    Click listener

--- Events.js
+++ Events.js
@@ -4,3 +4,6 @@ const child1 = document.querySelector(".child-1");
 const child2 = document.querySelector(".child-2");
 const child3 = document.querySelector(".child-3");
 
+child1.addEventListener("click", e => {
+    console.log(e.target);
+} );
