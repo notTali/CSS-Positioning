508cdd85353d5f9e43f681f4a8601ea99b68bbd5
notTali <mtalifhani54@gmail.com>
2022-04-22 20:06:27 +0200

    Updated element styles

--- DOM.js
+++ DOM.js
@@ -19,4 +19,21 @@ h1.style.padding = "5px 0px 5px 20px"
 section.append(h1);
 
 
-const button = document.createElement("p");
+
+const aTag = document.createElement("d");
+// aTag.setAttribute("href", "#");
+aTag.innerText = "Click Me!"
+aTag.style.padding = "0px 10px";
+aTag.style.color = "white";
+aTag.style.backgroundColor = "black";
+aTag.style.borderRadius = "10px";
+aTag.style.display = `flex`;
+aTag.style.alignItems = "center";
+aTag.style.justifyContent = "center";
+aTag.style.fontSize = "1.5rem"
+// const txt = document.createElement("p");
+
+// aTag.appendChild(txt);
+// console.log(aTag);
+
+section.appendChild(aTag);
